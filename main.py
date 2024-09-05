d192d652f00fce54d4e2b9379644097cf6bfb1ee
Angel-CSC <programminggom587@gmail.com>
2024-09-05 16:14:08 -0500

    making changes to not delete og

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-from routes import oxalotl
+from routes import oxalotl, anime
 
 app = FastAPI()
 
-
+app.include_router(anime.router)
 app.include_router(oxalotl.router)
 
 
