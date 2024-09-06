f1c46cc99966b6a33b1cc22c403947faedfd832a
Pranav <anupragol05@users.noreply.github.com>
2024-09-05 21:21:46 -0500

    Changes v3

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 # pg
 
 from fastapi import FastAPI
-from routes import oxalotl, anime, animals
+from routes import anime, animals
 
 app = FastAPI()
 
 app.include_router(animals.router)
 app.include_router(anime.router)
-app.include_router(oxalotl.router)
 
 
 @app.get("/")
