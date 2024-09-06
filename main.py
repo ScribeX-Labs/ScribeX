77fea4aafba12d9d20f6ad9c75d0b6d1b700e92b
emmalaura <108916939+emmalaura@users.noreply.github.com>
2024-09-05 22:49:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from fastapi import FastAPI
 
-from routes import oxalotl, anime, animals
+from routes import oxalotl, anime, animals,dogfacts
 
 app = FastAPI()
 
 app.include_router(animals.router)
 app.include_router(anime.router)
 app.include_router(oxalotl.router)
+app.include_router(dogfacts.router)
 
 
 @app.get("/")
 async def root():
     return {"message": "Scribe Test API"}
+
