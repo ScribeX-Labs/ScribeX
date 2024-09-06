52c08ec04d9344d5ce7aabe5c7016e038639d7c1
iiEthan <ethangibbs2@icloud.com>
2024-09-05 19:39:46 -0500

    Added dog route

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-from routes import oxalotl, anime
+from routes import oxalotl, anime, dogs
 
 app = FastAPI()
 
 app.include_router(anime.router)
 app.include_router(oxalotl.router)
-
+app.include_router(dogs.router)
 
 @app.get("/")
 async def root():
