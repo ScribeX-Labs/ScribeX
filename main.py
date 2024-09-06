286411d4217db2d9f633b26f5b50e76e1c0cc4a0
emmalaura <emmalaura0422@gmail.com>
2024-09-05 22:42:58 -0500

    Main Added dogfacts API

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 
-from routes import oxalotl, anime, animals
+from routes import oxalotl, anime, animals,dogfacts
 
 app = FastAPI()
 
 app.include_router(animals.router)
 app.include_router(anime.router)
 app.include_router(oxalotl.router)
+app.include_router(dogfacts.router)
 
 
 @app.get("/")
