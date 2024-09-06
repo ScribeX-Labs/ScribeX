deb289f8f7976f9fc320858284c2332147b6264f
emmalaura <emmalaura0422@gmail.com>
2024-09-05 22:47:16 -0500

    fixed dogfacts.py

--- routes/dogfacts.py
+++ routes/dogfacts.py
@@ -5,7 +5,7 @@ router = APIRouter()
 
 
 @router.get("/dogfacts")
-async def get_oxalotl_fact():
+async def get_dog_facts():
     url = "https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1"
     response = requests.get(url)
     quote = response.json()
