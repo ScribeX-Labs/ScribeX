97d2b764eabc95479ef3f210019b173b21d10697
Pranav <anupragol05@users.noreply.github.com>
2024-09-05 21:07:10 -0500

    Added more changes

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 # pg
 
 from fastapi import FastAPI
-from routes import anime
+from routes import oxalotl, anime, animals
 
 app = FastAPI()
 
-
+app.include_router(animals.router)
 app.include_router(anime.router)
+app.include_router(oxalotl.router)
 
 
 @app.get("/")
 async def root():
-    return {"message": "Scribe Test API"}
+    return {"message": "Scribe Test API"} 
---
+++ routes/oxalotl.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+import requests
+
+router = APIRouter()
+
+
+@router.get("/oxalotl")
+async def get_oxalotl_fact():
+    url = "https://theaxolotlapi.netlify.app/"
+    response = requests.get(url)
+    quote = response.json()
+    return quote
+
